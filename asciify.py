49670a385bcafc96958b1608b92c36d0855bc397
percepmoid <nikrus94@gmail.com>
2018-10-29 00:01:49 -0500

    updated ascii.py
    first working update

--- asciify.py
+++ asciify.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
-
-ASCII_SHADING_CHARS = ['M', 'N', 'F', 'V', '$', 'I', '*', ':']  # from darkest to lightest
+ASCII_SHADING_CHARS = ['M', 'W', 'N', 'Q', 'B', 'H', 'K', 'R', '#', 'E', 'D', 'F', 'X', 'O', 'A', 'P', 'G', 'U', 'S',
+                       'V', 'Z', 'Y', 'C', 'L', 'T', 'J', '$', 'I', '*', ':', '.', ' ']  # from darkest to lightest
 
 
 class Asciify:
-    def __init__(self, image, new_width=500):
-        self.width, self.height = image.size  # image.size returns a 2 tuple (width, height) in pixels
+    def __init__(self, img, new_width=500):
+        self.width, self.height = img.size  # image.size returns a 2 tuple (width, height) in pixels
         self._nw = new_width
         self._nh = int(new_width * self.height / self.width)
-        self.im = image
+        self.im = img
 
     def grayify_and_resize(self):
         """
@@ -28,7 +28,7 @@ class Asciify:
 
         return result_list
 
-    def ascii_map(self, im_list, color_width=32):
+    def ascii_map(self, im_list, color_width=int(255 / len(ASCII_SHADING_CHARS )+1)):
         """
         Maps an ascii shading character to a pixel of each frame of the GIF
         :param im_list: a list of black and white Image objects
@@ -38,25 +38,29 @@ class Asciify:
 
         ascii_image_list = []  # unformatted ascii images; needs to be broken into proper rows and columns
         result_list = []  # ascii_image_list broken into proper rows and columns; how convinient
-        for image in im_list:
-            pixels = image.getdata()  # color data on every pixel per image
+        for im in im_list:
+            pixels = im.getdata()  # color data on every pixel per image
             append_list = []  # temporary list to append to ascii_image_list
             for pixel_value in pixels:
-                append_list.append(ASCII_SHADING_CHARS[pixel_value//color_width])  # 'replace' pixel with ascii char
+                index = int(pixel_value // color_width)
+                if index == len(ASCII_SHADING_CHARS):
+                    append_list.append(ASCII_SHADING_CHARS[-1])
+                else:
+                    append_list.append(ASCII_SHADING_CHARS[index])  # 'replace' pixel with ascii char
             ascii_image_list.append(append_list)  # adds an element to ascii_image_list containing every pixel for image
 
         for ascii_image in ascii_image_list:
-            pass
+            ascii_string = "".join(ascii_image)
+            result_list.append([ascii_string[index:index + self._nw]
+                                for index in range(0, len(ascii_string), self._nw)])
 
-        return ascii_image_list
+        return result_list
 
 
 if __name__ == "__main__":
-    asciify_im = Asciify(Image.open("earth.gif"), 10)
+    asciify_im = Asciify(Image.open("earth.gif"), 500)
     gif_list = asciify_im.grayify_and_resize()
-    char_list = asciify_im.ascii_map(gif_list)
+    ascii_images = asciify_im.ascii_map(gif_list)
     outfile = open("outfile.txt", 'w')
-    for list in char_list:
-        outfile.write("".join(list))
-        print(len(char_list[0]))
-    outfile.close()
+    for image in ascii_images:
+        outfile.write("\n".join(image) + '\n\n')
