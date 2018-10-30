ea03057bfda131c14b288c3aabc9f4828bfccb3b
percepmoid <nikrus94@gmail.com>
2018-10-30 07:46:33 -0500

    updated ascii.py
    updated docstrings

--- asciify.py
+++ asciify.py
@@ -1,7 +1,12 @@
 from PIL import Image, ImageFont, ImageDraw
 
+
+__author__ = 'Nikolas'
+
+
 ASCII_SHADING_CHARS = ['M', 'W', 'N', 'Q', 'B', 'H', 'K', 'R', '#', 'E', 'D', 'F', 'X', 'O', 'A', 'P', 'G', 'U', 'S',
                        'V', 'Z', 'Y', 'C', 'L', 'T', 'J', '$', 'I', '*', ':', '.', ' ']  # from darkest to lightest 32
+#ASCII_SHADING_CHARS = ASCII_SHADING_CHARS[::-1]
 
 
 class Asciify:
@@ -38,8 +43,8 @@ class Asciify:
 
         ascii_image_list = []  # unformatted ascii images; needs to be broken into proper rows and columns
         result_list = []  # ascii_image_list broken into proper rows and columns; how convinient
-        for im in im_list:
-            pixels = im.getdata()  # color data on every pixel per image
+        for image in im_list:
+            pixels = image.getdata()  # color data on every pixel per image
             append_list = []  # temporary list to append to ascii_image_list
             for pixel_value in pixels:
                 index = int(pixel_value // color_width)
@@ -57,29 +62,31 @@ class Asciify:
         return result_list
 
     def gifify(self, ascii_image_list):
-        font = ImageFont.truetype('ascii.ttf', 8)
-        ascii_images = ['\n'.join(image) for image in ascii_image_list]
-        pil_gifs = [Image.new('RGBA', (self._nw*5, self._nh*11)) for ascii_image in ascii_images]
-        draw_gifs = [ImageDraw.Draw(pil_gif) for pil_gif in pil_gifs]
-        for draw_gif in draw_gifs:
-            draw_gif.text((0,0), ascii_images[draw_gifs.index(draw_gif)], (255,255,255), font=font)
-        gif = Image.new('RGBA', size=(self._nw*5, self._nh*11))
-        gif.save('ascii.gif', save_all=True, loop=0, append_images=pil_gifs)
+        """
+        Return the ascii strings to .gif format for use in a traditional image viewer.
+        :param ascii_image_list: A list of ascii 'pixel' images
+        :returns: None
+        """
 
-        resize_list = []
-        gif_big = Image.open('ascii.gif')
-        for i in range(gif_big.n_frames - 1):
-            resize_list.append(gif_big.resize((self._nw, self._nh)))
-            gif_big.seek(gif_big.tell() + 1)
-        resized_gif = Image.new('RGBA', (self._nw, self._nh))
-        resized_gif.save('ascii_resized.gif', save_all=True, loop=0, append_images=resize_list)
+        font = ImageFont.truetype('ascii.ttf', 7)  # set font and font size
+        ascii_image_strings = ['\n'.join(image) for image in ascii_image_list]
+        # Create n PIL images, where n is the number of frames in the original gif
+        pil_gifs = [Image.new('RGBA', (self._nw*4, self._nh*10)) for i in range(len(ascii_image_strings))]
+        draw_gifs = [ImageDraw.Draw(pil_gif) for pil_gif in pil_gifs]  # create a PIL ImageDraw for the new frames
+        for draw_gif in draw_gifs:  # write the ascii strings to each frame
+            draw_gif.text((0, 0), ascii_image_strings[draw_gifs.index(draw_gif)], (255, 255, 255), font=font)
+        save_as = Image.new('RGBA', size=(self._nw*4, self._nh*10))  # create a new PIL Image to save the frames to
+        save_as.save('ascii.gif', save_all=True, loop=0, duration=42, append_images=pil_gifs[1:-1])  # save it
 
 
 if __name__ == "__main__":
-    asciify_im = Asciify(Image.open("earth.gif"), 200)
+    im = Image.open("trippy.gif")
+    asciify_im = Asciify(im, 110)
     gif_list = asciify_im.grayify_and_resize()
     ascii_images = asciify_im.ascii_map(gif_list)
-    gif = asciify_im.gifify(ascii_images)
-    #outfile = open("outfile.txt", 'w')
-    #for image in ascii_images:
+    asciify_im.gifify(ascii_images)
+
+    # Debugging ascii image creation #
+    # outfile = open("outfile.txt", 'w')
+    # for image in ascii_images:
     #    outfile.write("\n".join(image) + '\n\n')
